aefbb2940ce5181fce57e43705426b8c994afbcd
Riyaz Alvi <89968985+alvisahab8810@users.noreply.github.com>
2025-06-26 17:17:53 +0530

    changes done

--- pages/api/sales/quotation/create.js
+++ pages/api/sales/quotation/create.js
@@ -236,7 +236,11 @@ export default async function handler(req, res) {
     await newQuotation.save();
 
     // Generate PDF
-    const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+});
+
     const page = await browser.newPage();
     await page.setContent(previewHTML);
     const pdfBuffer = await page.pdf({ format: "A4" });
